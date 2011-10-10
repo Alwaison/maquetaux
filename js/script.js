61b9a6267b9cdfbeb1aa7d358f36ff384ef7ae9d
Joan Fernandez <joan@joanfernandez.es>
2011-10-10 11:26:18 +0200

    Archivos de HTML5 boilerplate

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* Author: 
+
+*/
+
+
+/**
+ * Funcion de test en document ready de jQuery
+ */
+function testFunction(){
+	//console.log('Test');
+}
+
+
+/**
+ * Document ready de jQuery.
+ */
+jQuery(document).ready(function(){
+	testFunction();
+});
